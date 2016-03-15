5fee840187fc5c3ad1c7360a3c8b7f01257683e9
vignesh-m <vicky.mano@gmail.com>
2016-03-15 18:33:42 +0530

    simple graph disaply done

---
+++ gui/server.py
@@ -0,0 +1,18 @@
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+import SocketServer
+import os.path
+
+PORT = 8000
+
+
+class MySimpleHTTPRequestHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            if (not os.path.isfile('index.html') and os.path.isfile('gui/index.html')):
+                self.path = '/gui'
+        return SimpleHTTPRequestHandler.do_GET(self)
+
+httpd = SocketServer.TCPServer(("", PORT), MySimpleHTTPRequestHandler)
+
+print "serving at port", PORT
+httpd.serve_forever()
