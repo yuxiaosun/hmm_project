e0394aaf0241db67ef7a9e4ecf14fdd6729b7b3c
war-owl <sunilkmrjs@gmail.com>
2016-04-06 23:20:52 +0530

    modified gaamamixcal

--- continous_hmm.py
+++ continous_hmm.py
@@ -1,17 +1,16 @@
 import numpy
-
 class  continuoushmm():
     """for looping over states j iterator is used
     for looping over observations t iterator is used
     for looping over clusterms  k is used
     """
-    def __init__(self,n,m,p,a=None,means=None,covars=None,g=None,start_prob=None,uniform_initialize=False):
+    def __init__(self,n,m,p,a=None,means=None,covarsmatrix=None,g=None,start_prob=None,uniform_initialize=False):
         self.n=n
         self.m=m
         self.p=p
         self.a=a
-        self.means=means
-        self.covars=covars
+        self.meansmatrix=means
+        self.covarsmatrix=covarsmatrix
         self.g=g
         self.pi=start_prob
         if uniform_initialize:
@@ -21,29 +20,42 @@ class  continuoushmm():
     def initialize(self):
         self.pi= numpy.ones( (self.n) )* (1/self.n)
         self.a = numpy.ones((self.n,self.n))* (1/self.n)
-        self.mean =  numpy.zeros( (self.n,self.m,self.p) )
-        self.covars= [[ numpy.matrix(numpy.ones((self.p,self.p))) for k in xrange(self.m)] for j in xrange(self.n)]
+        self.meansmatrix =  numpy.zeros( (self.n,self.m,self.p) )
+        x=numpy.zeros( (self.p,self.p))
+        numpy.fill_diagonal(x,1)
+        self.covarsmatrix= [[ numpy.matrix(x) for k in xrange(self.m)] for j in xrange(self.n)]
         self.g= numpy.ones ( (self.n,self.m))*1/self.m
 
 
     def gaamamixcal(self,alpha,beta,observations):
+        self.kernelvaluescal(observations)
         self.gaamamix= numpy.zeros( (len(observations), self.n, self.m) )
-        gaama=self._gaamacal(alpha,beta)
+        #print self.gaamamix
+        gaama=self._gaamacal(alpha,beta,observations)
         for t in xrange(0,len(observations)):
             for j in xrange(self.n):
                 numer=0
                 denom=0
                 for k in xrange(self.m):
-                    numer=g[j][k]*self.kernerlvalues[j][k][t]
+                    #print self.g[j][k]
+                    #print self.kernelvalues[j][k][t]
+                    numer=self.g[j][k]*self.kernelvalues[j][k][t]
+                    #print numer
+                    #print numer
+                    #print numer
                     for kk in range(self.m):
-                        denom=denom+g[j][kk]*self.kernerlvalues[j][kk][t]
+                        denom=denom+self.g[j][kk]*self.kernelvalues[j][kk][t]
                     term2= numer/denom
-                    gaamamix[t][j][k]= gaama[j][t]*term2
+                    #print term2
+                    self.gaamamix[t][j][k]= gaama[j][t]*term2
+        #print self.gaamamix
         return self.gaamamix
 
 
-    def _gaamacal(self,alpha,beta):
-        gaama=numpy.zeros((self.n,self.m))
+    def _gaamacal(self,alpha,beta,observations):
+        gaama=numpy.zeros((self.n,len(observations)))
+        #print gaama
+        #print len(observations)
 
         for t in xrange(0,len(observations)):
             numer=0
@@ -51,7 +63,9 @@ class  continuoushmm():
                 numer= alpha[j][t]*beta[j][t]
                 denom=0
                 for jj in xrange(0,self.n):
-                    denom=+ alpha[jj][t]*beta[jj][t]
+                    denom=denom+alpha[jj][t]*beta[jj][t]
+                #print numer
+                #print denom
                 gaama[j][t]= numer/denom
 
         return gaama
@@ -61,15 +75,26 @@ class  continuoushmm():
         for j in xrange(self.n):
             for k in xrange(self.m):
                 for t in xrange(len(observations)):
-                    self.kernelvalues[j][k][t]= self._gaussianpdf(mean,covarmatrix,observation)
+                    #print self.meansmatrix[j][k]
+                    #print self.covarsmatrix[j][k]
+                    #print observations[t]
+                    self.kernelvalues[j][k][t]= self._gaussianpdf(self.meansmatrix[j][k],self.covarsmatrix[j][k],observations[t])
 
-        return self.kernelvalues
 
     def _gaussianpdf(self,mean,covarmatrix,observation):
         covar_det = numpy.linalg.det(covarmatrix)
+        #print mean
+        #print type(covarmatrix)
+        #print observation
+        #print covar_det
         c =  (2.0*numpy.pi)**(float(self.p/2.0)) * (covar_det)**(0.5)
         c=1/c
-        pdfval = c * numpy.exp(-0.5 * numpy.dot( numpy.dot((observation-mean),covarmatrix.I), (observation-mean)) )
+        print c
+        print numpy.dot( numpy.dot((observation-mean),covarmatrix.I), (observation-mean))
+        pdfval = c *  numpy.exp(-0.5 * numpy.dot( numpy.dot((observation-mean),covarmatrix.I), (observation-mean)) )
+        # returns zero for very large numbers . how to fix ?
+        print pdfval
+        return pdfval
 
 
         #calucaltion of gaaussian mixture pdf
@@ -103,5 +128,3 @@ class  continuoushmm():
                     bjt=bjt+self.g[j][k]*self.kernelvalues[j][k][t]
                 b[j][t]=bjt
         return self.b
-
-hmm=continuoushmm(n=6,m=8,p=4,uniform_initialize=True)
